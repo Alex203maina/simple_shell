1daa0b90fea93e5e0ea847382ce0aabeccce27ad
Elisha-Sani <elishamuuo1@gmail.com>
2023-12-12 06:16:52 -0800

    Shell 02

--- simple_shell_02.c
+++ simple_shell_02.c
@@ -5,6 +5,11 @@
 
 #define MAX_CMD_LENGTH 1024
 #define MAX_ARGS 64
+/**
+ * struct command - Command structure
+ * @name: Name of the command
+ * @args: Arguments of the command
+ */
 
 struct command
 {
@@ -12,6 +17,11 @@ struct command
 	char *args[MAX_ARGS];
 };
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 	char *cmd_name;
 	char *cmd_args[MAX_ARGS];
@@ -20,9 +30,11 @@ int main(void)
 
 	printf("$ ");
 
+	/* Get command from user */
 	fgets(cmd, MAX_CMD_LENGTH, stdin);
 	cmd_name = cmd;
 
+	/* Tokenize the command */
 	char *token = strtok(cmd_name, " ");
 	int i = 0;
 	while (tiken != NULL && i < MAX_ARGS)
@@ -31,12 +43,14 @@ int main(void)
 		token = strtok(NULL, " ");
 	}
 
+	/* Execute the command */
 	if (execvp(cmd_name, cmd_args) == -1)
 	{
 		perror("execvp");
 		return (1);
 	}
 
+	/* Wait for the completion of command */
 	wait(&status);
 
 	return (0);
