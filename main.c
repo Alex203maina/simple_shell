949877cb2e9126ec7203b912fe9a5ec201e502c5
Elisha-Sani <elishamuuo1@gmail.com>
2023-12-12 04:35:49 -0800

    Adding 2 numbers

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Start point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 10;
+	int b = 5;
+
+	c = a + b;
+	return (c);
+
+	return (0);
+}
